46c6d367f4eb652aca6f61e1d5e398a1afcd1e66
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-01-16 00:44:21 +0530

    style: prettier lint issues fixed
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from "tsdown";
+import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm"],
-  outDir: "dist",
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  outDir: 'dist',
   dts: true,
   clean: true,
   sourcemap: true,
   treeshake: true,
-  external: ["maplibre-gl"],
+  external: ['maplibre-gl'],
   noExternal: [
-    "@mapbox/geojson-rewind",
-    "@sakitam-gis/rbush",
-    "@sakitam-gis/vis-engine",
-    "wind-gl-core",
+    '@mapbox/geojson-rewind',
+    '@sakitam-gis/rbush',
+    '@sakitam-gis/vis-engine',
+    'wind-gl-core',
   ],
 });
